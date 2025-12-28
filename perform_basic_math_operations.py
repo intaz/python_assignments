b77f7dba17d954d3f18ceced2e5319050de56d90
intaz <intazahmed@gmail.com>
2025-12-28 21:54:22 +0530

    1st assignment

---
+++ perform_basic_math_operations.py
@@ -0,0 +1,12 @@
+firstNumber = int(input("Enter the first number: "))
+secondNumber = int(input("Enter the second number: "))
+
+addition = firstNumber + secondNumber
+subtraction = firstNumber - secondNumber
+multiplication = firstNumber * secondNumber
+division = firstNumber / secondNumber
+print("\n")
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
